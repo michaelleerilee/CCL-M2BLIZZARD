245b8f55fed266e799be7616231d570c94368c00
Michael Lee Rilee <mike@rilee.net>
2018-12-10 15:33:06 -0500

    Added some sophistication to the driver.

--- ccl_dask_blizzard_nc4.py
+++ ccl_dask_blizzard_nc4.py
@@ -5,14 +5,20 @@ import numpy as np
 from load_ncdf_for_matsuzawa import input_array, m2_datafiles
 
 from ccl_marker_stack    import ccl_dask
+from time                import time
 
 M2T1NXFLX_base,M2T1NXFLX_datafiles\
     = m2_datafiles(M2DATASET='M2T1NXFLX')
 
 nfiles = len(M2T1NXFLX_datafiles)
 
-# run_case = 'TestCase1'
-run_case = 'TestCase2'
+run_case = 'TestCase1'
+# run_case = 'TestCase2'
+# run_case = 'TestCase3-1'
+# run_case = 'TestCase3-2'
+# run_case = 'TestCase3-3'
+# run_case = 'TestCase3-4'
+# run_case = 'TestCase4'
 run_case_set = False
 if 'run_results' not in locals() and 'run_results' not in globals():
     run_results = {}
@@ -33,13 +39,58 @@ if run_case == 'TestCase2':
                    [0,1]\
     ]
 # 89
-    
+
+if run_case == 'TestCase3-1':
+    run_case_set = True
+    findex_ranges=[\
+                   [0,1,2]\
+    ]
+
+if run_case == 'TestCase3-2':
+    run_case_set = True
+    findex_ranges=[\
+                   [0],\
+                   [1,2]\
+    ]
+
+if run_case == 'TestCase3-3':
+    run_case_set = True
+    findex_ranges=[\
+                   [0,1],\
+                   [2]\
+    ]
+
+if run_case == 'TestCase3-4':
+    run_case_set = True
+    findex_ranges=[\
+                   [0]\
+                   ,[1]\
+                   ,[2]\
+    ]
+
+if run_case == 'TestCase4':
+    run_case_set = True
+    findex_ranges=[\
+                   [0,1]\
+                   ,[2,3]\
+                   ,[4,5]\
+                   ,[6,7]\
+    ]
+
+
 if run_case_set is False:
     print 'run_case is not set, quitting!'
     quit()
 
 def loader(fidx_range,argList):
     return input_array(fidx_range).visibility_i[:]
+
+###########################################################################
+# Start timing
+
+time_force_execution            = False
+time_measurements               = {}
+time_measurements['time_start'] = time()
 
 ###########################################################################
 # Load
@@ -54,6 +105,11 @@ if True:
     ccl_dask_object = ccl_dask()
     ccl_dask_object.load_data_segments_with_loader(loader,findex_ranges,[()])
 
+if time_force_execution:
+    for future in ccl_dask_object.data_seg:
+        future.result()
+time_measurements['time_loader_1'] = time()
+
 # Diagnostics
 if False:
     print 'ccl_dask_object.data_segs',ccl_dask_object.data_segs
@@ -65,6 +121,12 @@ if False:
 if True:
     ccl_dask_object.make_blizzard_stacks(thresh_mnmx)
     # ccl_dask_object.make_stacks(thresh_mnmx)
+
+    if time_force_execution:
+        for future in ccl_dask_object.segs_blizz_mask_results:
+            future.result()
+    time_measurements['time_blizz_mask_results_1'] = time()
+    
     ccl_dask_object.shift_labels()
     ccl_dask_object.make_translations()
     ccl_dask_object.apply_translations()
@@ -80,6 +142,8 @@ if True:
     print 'collect_simplified_results'
     ccl_dask_object.collect_simplified_results()
 
+    time_measurements['time_ccl_simplified_results_1'] = time()
+    
     print 'ccl_dask calculation done'
 
 # Diagnostics looking at the results of the data load
@@ -135,6 +199,12 @@ if True:
 
 print 'ccl_dask_blizzard_nc4 completing '+run_case
 run_results[run_case] = ccl_dask_object
+print 'wall clock timings'
+print 'load                ',time_measurements['time_loader_1']                 - time_measurements['time_start']
+print 'blizz mask+load     ',time_measurements['time_blizz_mask_results_1']     - time_measurements['time_start']
+print 'ccl+blizz mask+load ',time_measurements['time_ccl_simplified_results_1'] - time_measurements['time_start']
+print 'blizz mask          ',time_measurements['time_blizz_mask_results_1']     - time_measurements['time_loader_1']
+print 'ccl                 ',time_measurements['time_ccl_simplified_results_1'] - time_measurements['time_blizz_mask_results_1']
 print 'done'
 
 # Note, if we have to do the 3-hour blizzard calculation w/o CCL, then we can monkey with the load_data_segments to
