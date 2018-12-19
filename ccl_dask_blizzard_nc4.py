9e3527dd8a9b30ea88d961a5c51e3958cd8fa806
Michael Rilee <Michael.L.Rilee-1@nasa.gov>
2018-12-18 22:19:39 -0500

    Added items for running in NCCS Discover's queues and IO.

--- ccl_dask_blizzard_nc4.py
+++ ccl_dask_blizzard_nc4.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
 
+# Try ./ccl_dask_blizzard_nc4.py -r TestCase1
+
+import argparse
+import sys
 
 import numpy as np
 # from load_for_ccl_inputs import load_for_ccl_inputs
@@ -7,80 +12,127 @@ from load_ncdf_for_matsuzawa import input_array, m2_datafiles
 from ccl_marker_stack    import ccl_dask
 from time                import time
 
+parser = argparse.ArgumentParser(description='Drive a dask-based parallel CCL blizzard detection & tracking analysis.')
+
+parser.add_argument('-r','--run_case'\
+                    ,dest='run_case'\
+                    ,type=str\
+                    ,required=False\
+                    ,help='Use an embedded test case: TestCase1, TestCase2, TestCase3-[1..4], TestCase4, Year1, Year1a, Year1b')
+parser.set_defaults(run_case=None)
+
+parser.add_argument('-o','--output'\
+                    ,dest='output_dirpath'\
+                    ,type=str\
+                    ,required=False\
+                    ,help='Set the directory for the output.')
+parser.set_defaults(output_dirpath='./')
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit()
+
+args=parser.parse_args()
+
 M2T1NXFLX_base,M2T1NXFLX_datafiles\
     = m2_datafiles(M2DATASET='M2T1NXFLX')
 
 nfiles = len(M2T1NXFLX_datafiles)
 
-run_case = 'TestCase1'
-# run_case = 'TestCase2'
-# run_case = 'TestCase3-1'
-# run_case = 'TestCase3-2'
-# run_case = 'TestCase3-3'
-# run_case = 'TestCase3-4'
-# run_case = 'TestCase4'
+output_dirpath = args.output_dirpath
+
+run_case     = args.run_case
+run_case_set = run_case is None
 run_case_set = False
-if 'run_results' not in locals() and 'run_results' not in globals():
-    run_results = {}
 
-# Test case 1.
-if run_case == 'TestCase1':
-    run_case_set = True
-    findex_ranges=[\
-                   [0],\
-                   [1] \
-    ]
-# 89
+if run_case is not None:
+    # run_case = 'TestCase1'
+    # run_case = 'TestCase2'
+    # run_case = 'TestCase3-1'
+    # run_case = 'TestCase3-2'
+    # run_case = 'TestCase3-3'
+    # run_case = 'TestCase3-4'
+    # run_case = 'TestCase4'
+    # run_case = 'Year1'
+    # run_case = 'Year1a'
+    # run_case = 'Year1b'
     
-# Test case 2.
-if run_case == 'TestCase2':
-    run_case_set = True
-    findex_ranges=[\
-                   [0,1]\
-    ]
-# 89
+    if 'run_results' not in locals() and 'run_results' not in globals():
+        run_results = {}
 
-if run_case == 'TestCase3-1':
-    run_case_set = True
-    findex_ranges=[\
-                   [0,1,2]\
-    ]
-
-if run_case == 'TestCase3-2':
-    run_case_set = True
-    findex_ranges=[\
-                   [0],\
-                   [1,2]\
-    ]
-
-if run_case == 'TestCase3-3':
-    run_case_set = True
-    findex_ranges=[\
-                   [0,1],\
-                   [2]\
-    ]
-
-if run_case == 'TestCase3-4':
-    run_case_set = True
-    findex_ranges=[\
-                   [0]\
-                   ,[1]\
-                   ,[2]\
-    ]
-
-if run_case == 'TestCase4':
-    run_case_set = True
-    findex_ranges=[\
-                   [0,1]\
-                   ,[2,3]\
-                   ,[4,5]\
-                   ,[6,7]\
-    ]
-
-
+    # Test case 1.
+    if run_case == 'TestCase1':
+        run_case_set = True
+        findex_ranges=[\
+                       [0],\
+                       [1] \
+        ]
+        # 89
+        
+    # Test case 2.
+    if run_case == 'TestCase2':
+        run_case_set = True
+        findex_ranges=[\
+                       [0,1]\
+        ]
+    # 89
+    
+    if run_case == 'TestCase3-1':
+        run_case_set = True
+        findex_ranges=[\
+                       [0,1,2]\
+        ]
+    
+    if run_case == 'TestCase3-2':
+        run_case_set = True
+        findex_ranges=[\
+                       [0],\
+                       [1,2]\
+        ]
+    
+    if run_case == 'TestCase3-3':
+        run_case_set = True
+        findex_ranges=[\
+                       [0,1],\
+                       [2]\
+        ]
+    
+    if run_case == 'TestCase3-4':
+        run_case_set = True
+        findex_ranges=[\
+                       [0]\
+                       ,[1]\
+                       ,[2]\
+        ]
+    
+    if run_case == 'TestCase4':
+        run_case_set = True
+        findex_ranges=[\
+                       [0,1]\
+                       ,[2,3]\
+                       ,[4,5]\
+                       ,[6,7]\
+        ]
+    if run_case == 'Year1':
+        run_case_set = True
+        # By fours, except the last.
+        findex_ranges=[range(4*i,4*i+4) for i in range(90)]+[range(360,365)]
+    
+    if run_case == 'Year1a':
+        run_case_set = True
+        # By fours, except the last.
+        findex_ranges=[range(4*i,4*i+4) for i in range(4)]
+    
+    if run_case == 'Year1b':
+        run_case_set = True
+        # By fours, except the last.
+        findex_ranges=[range(4*i,4*i+4) for i in range(8)]    
+        
 if run_case_set is False:
     print 'run_case is not set, quitting!'
     quit()
+else:
+    print 'executing run_case = ',run_case
 
 def loader(fidx_range,argList):
     return input_array(fidx_range).visibility_i[:]
@@ -102,10 +154,13 @@ thresh_mnmx = (1.0e-3,1.0)
 
 # Calculation: Load data
 if True:
+    print 'initialize ccl_dask_object'
     ccl_dask_object = ccl_dask()
+    print 'load data'
     ccl_dask_object.load_data_segments_with_loader(loader,findex_ranges,[()])
 
 if time_force_execution:
+    print 'forcing execution'
     for future in ccl_dask_object.data_seg:
         future.result()
 time_measurements['time_loader_1'] = time()
@@ -119,19 +174,25 @@ if False:
     
 # Calculations
 if True:
+    print 'make blizzard stacks'
     ccl_dask_object.make_blizzard_stacks(thresh_mnmx)
     # ccl_dask_object.make_stacks(thresh_mnmx)
 
     if time_force_execution:
+        print 'forcing execution'
         for future in ccl_dask_object.segs_blizz_mask_results:
             future.result()
     time_measurements['time_blizz_mask_results_1'] = time()
-    
+
+    print 'make labels'
     ccl_dask_object.shift_labels()
+    print 'make translations'
     ccl_dask_object.make_translations()
+    print 'apply translations'
     ccl_dask_object.apply_translations()
 
     # serial collect
+    print 'serial collect all results'
     ccl_dask_object.collect_all_results()
 
     # Simplify the labels so they are sequential without gaps.
@@ -143,6 +204,10 @@ if True:
     ccl_dask_object.collect_simplified_results()
 
     time_measurements['time_ccl_simplified_results_1'] = time()
+
+    # save to disk
+    
+    time_measurements['time_ccl_simplified_results_2'] = time()
     
     print 'ccl_dask calculation done'
 
@@ -191,6 +256,10 @@ if True:
         ,ccl_dask_object.ccl_results_simplified_labels[iseg].m_results_simple[isl][r0:r1,c0:c1]
     np.set_printoptions(threshold=1000,linewidth=75)
 
+if True:
+    print 'Output the CCL labels.'
+    ccl_dask_object.save_simplified_results(output_dirpath)
+
 if False:
     ccl_dask_object.diagnose_parallel_simplify_0()
 
@@ -200,11 +269,13 @@ if True:
 print 'ccl_dask_blizzard_nc4 completing '+run_case
 run_results[run_case] = ccl_dask_object
 print 'wall clock timings'
-print 'load                ',time_measurements['time_loader_1']                 - time_measurements['time_start']
-print 'blizz mask+load     ',time_measurements['time_blizz_mask_results_1']     - time_measurements['time_start']
-print 'ccl+blizz mask+load ',time_measurements['time_ccl_simplified_results_1'] - time_measurements['time_start']
-print 'blizz mask          ',time_measurements['time_blizz_mask_results_1']     - time_measurements['time_loader_1']
-print 'ccl                 ',time_measurements['time_ccl_simplified_results_1'] - time_measurements['time_blizz_mask_results_1']
+print 'load                        ',time_measurements['time_loader_1']                 - time_measurements['time_start']
+print 'blizz mask+load             ',time_measurements['time_blizz_mask_results_1']     - time_measurements['time_start']
+print 'ccl+blizz mask+load         ',time_measurements['time_ccl_simplified_results_1'] - time_measurements['time_start']
+print 'ccl+blizz mask+load+output  ',time_measurements['time_ccl_simplified_results_2'] - time_measurements['time_start']
+print 'blizz mask                  ',time_measurements['time_blizz_mask_results_1']     - time_measurements['time_loader_1']
+print 'ccl                         ',time_measurements['time_ccl_simplified_results_1'] - time_measurements['time_blizz_mask_results_1']
+print 'output                      ',time_measurements['time_ccl_simplified_results_2'] - time_measurements['time_ccl_simplified_results_1']
 print 'done'
 
 # Note, if we have to do the 3-hour blizzard calculation w/o CCL, then we can monkey with the load_data_segments to
